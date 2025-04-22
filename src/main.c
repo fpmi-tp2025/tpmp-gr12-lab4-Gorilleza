72c6eea0b3f509a7f761d0d52736970c2a66512f
Eziz Hojamuhammedow <ezizhojamuhammedow.5527@gmail.com>
2025-04-22 19:50:26 +0300

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "auth.h"
+#include "country.h"
+#include "region.h"
+
+void print_menu() {
+    printf("\nCountry Management System\n");
+    printf("1. Login\n");
+    printf("2. List Countries\n");
+    printf("3. Add Country\n");
+    printf("4. List Regions of Country\n");
+    printf("5. Show Average Region Population\n");
+    printf("6. Show Total Population\n");
+    printf("7. Exit\n");
+    printf("Enter your choice: ");
+}
+
+int main() {
+    if (!init_database("countries.db")) {
+        printf("Cannot open database!\n");
+        return 1;
+    }
+    
+    int choice;
+    while (1) {
+        print_menu();
+        scanf("%d", &choice);
+        
+        switch (choice) {
+            case 1: {
+                char username[50], password[50];
+                printf("Username: ");
+                scanf("%s", username);
+                printf("Password: ");
+                scanf("%s", password);
+                
+                if (authenticate(username, password)) {
+                    printf("Login successful!\n");
+                } else {
+                    printf("Login failed!\n");
+                }
+                break;
+            }
+            case 2:
+                list_countries();
+                break;
+            case 3: {
+                if (!is_authenticated()) {
+                    printf("Please login first!\n");
+                    break;
+                }
+                
+                char name[100], capital[100];
+                long population;
+                
+                printf("Country name: ");
+                scanf("%s", name);
+                printf("Capital: ");
+                scanf("%s", capital);
+                printf("Population: ");
+                scanf("%ld", &population);
+                
+                if (add_country(name, capital, population)) {
+                    printf("Country added successfully!\n");
+                } else {
+                    printf("Failed to add country!\n");
+                }
+                break;
+            }
+            case 4: {
+                int country_id;
+                printf("Enter country ID: ");
+                scanf("%d", &country_id);
+                list_country_regions(country_id);
+                break;
+            }
+            case 5: {
+                int country_id;
+                printf("Enter country ID: ");
+                scanf("%d", &country_id);
+                double avg = get_average_region_population(country_id);
+                printf("Average population: %.2f\n", avg);
+                break;
+            }
+            case 6: {
+                long total = get_total_population();
+                printf("Total population: %ld\n", total);
+                break;
+            }
+            case 7:
+                close_database();
+                exit(0);
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+    
+    return 0;
+}
