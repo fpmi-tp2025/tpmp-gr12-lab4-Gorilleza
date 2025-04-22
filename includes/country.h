aa5b697fdf7b51c78248c70c9b45e6668ad5d522
Eziz Hojamuhammedow <ezizhojamuhammedow.5527@gmail.com>
2025-04-22 19:45:02 +0300

    Create country.h

---
+++ includes/country.h
@@ -0,0 +1,17 @@
+#ifndef COUNTRY_H
+#define COUNTRY_H
+
+typedef struct {
+    int id;
+    char name[100];
+    char capital[100];
+    long population;
+} Country;
+
+void list_countries();
+bool add_country(const char* name, const char* capital, long population);
+bool delete_country(int id);
+void list_country_regions(int country_id);
+double get_average_region_population(int country_id);
+
+#endif
