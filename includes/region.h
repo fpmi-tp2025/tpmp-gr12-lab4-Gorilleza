26562bce85af8a36b6eec626ba70a1a0c8ac948c
Eziz Hojamuhammedow <ezizhojamuhammedow.5527@gmail.com>
2025-04-23 08:55:11 +0300

    Update regoin.h

--- includes/region.h
+++ includes/region.h
@@ -1,6 +1,8 @@
 #ifndef REGION_H
 #define REGION_H
 
+#include <stdbool.h>
+
 bool add_region(int country_id, const char* name, long population);
 bool delete_region(int id);
 long get_total_population();
