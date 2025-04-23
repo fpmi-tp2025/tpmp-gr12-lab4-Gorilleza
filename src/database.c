afe9bef9411b091e80760bd9bb26054e93ef870a
Eziz Hojamuhammedow <ezizhojamuhammedow.5527@gmail.com>
2025-04-23 09:00:30 +0300

    Update database.c

--- src/database.c
+++ src/database.c
@@ -1,11 +1,16 @@
 #include <sqlite3.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include "database.h"
 
 static sqlite3 *db;
 
 bool init_database(const char* db_path) {
-    return sqlite3_open(db_path, &db) == SQLITE_OK;
+    if (sqlite3_open(db_path, &db) != SQLITE_OK) {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return false;
+    }
+    return true;
 }
 
 void close_database() {
