671b75cb699018532d9dbec262821a344a430978
Eziz Hojamuhammedow <ezizhojamuhammedow.5527@gmail.com>
2025-04-28 21:00:43 +0300

    Update region.h

--- includes/region.h
+++ includes/region.h
@@ -4,7 +4,8 @@
 #include <stdbool.h>
 
 bool add_region(int country_id, const char* name, long population);
-bool delete_region(int id);
+void list_country_regions(int country_id);
+double get_average_region_population(int country_id);
 long get_total_population();
 
 #endif
