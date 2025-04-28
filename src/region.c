d8cc6d94d6636a9654dcdc9c7dadc6544c86653a
Eziz Hojamuhammedow <ezizhojamuhammedow.5527@gmail.com>
2025-04-28 21:01:58 +0300

    Update region.c

--- src/region.c
+++ src/region.c
@@ -6,7 +6,7 @@
 
 bool add_region(int country_id, const char* name, long population) {
     sqlite3_stmt *stmt;
-    const char *sql = "INSERT INTO regions (country_id, name, population) VALUES (?, ?, ?);";
+    const char *sql = "INSERT INTO region (country_id, name, population) VALUES (?, ?, ?);";
     
     if (sqlite3_prepare_v2(get_db_connection(), sql, -1, &stmt, NULL) != SQLITE_OK) {
         printf("Cannot prepare statement: %s\n", sqlite3_errmsg(get_db_connection()));
@@ -22,24 +22,9 @@ bool add_region(int country_id, const char* name, long population) {
     return result;
 }
 
-bool delete_region(int id) {
-    sqlite3_stmt *stmt;
-    const char *sql = "DELETE FROM regions WHERE id = ?;";
-    
-    if (sqlite3_prepare_v2(get_db_connection(), sql, -1, &stmt, NULL) != SQLITE_OK) {
-        printf("Cannot prepare statement: %s\n", sqlite3_errmsg(get_db_connection()));
-        return false;
-    }
-    
-    sqlite3_bind_int(stmt, 1, id);
-    bool result = sqlite3_step(stmt) == SQLITE_DONE;
-    sqlite3_finalize(stmt);
-    return result;
-}
-
 void list_country_regions(int country_id) {
     sqlite3_stmt *stmt;
-    const char *sql = "SELECT id, name, population FROM regions WHERE country_id = ?;";
+    const char *sql = "SELECT id, name, population FROM region WHERE country_id = ?;";
     
     if (sqlite3_prepare_v2(get_db_connection(), sql, -1, &stmt, NULL) != SQLITE_OK) {
         printf("Cannot prepare statement: %s\n", sqlite3_errmsg(get_db_connection()));
@@ -50,39 +35,10 @@ void list_country_regions(int country_id) {
     
     printf("\nID\tRegion\t\tPopulation\n");
     while (sqlite3_step(stmt) == SQLITE_ROW) {
-        printf("%d\t%s\t%lld\n",  // Изменено с %ld на %lld
+        printf("%d\t%s\t%lld\n",
                sqlite3_column_int(stmt, 0),
                sqlite3_column_text(stmt, 1),
                sqlite3_column_int64(stmt, 2));
     }
     sqlite3_finalize(stmt);
-}
-
-double get_average_region_population(int country_id) {
-    sqlite3_stmt *stmt;
-    const char *sql = "SELECT AVG(population) FROM regions WHERE country_id = ?;";
-    double avg = 0.0;
-    
-    if (sqlite3_prepare_v2(get_db_connection(), sql, -1, &stmt, NULL) == SQLITE_OK) {
-        sqlite3_bind_int(stmt, 1, country_id);
-        if (sqlite3_step(stmt) == SQLITE_ROW) {
-            avg = sqlite3_column_double(stmt, 0);
-        }
-        sqlite3_finalize(stmt);
-    }
-    return avg;
-}
-
-long get_total_population() {
-    sqlite3_stmt *stmt;
-    const char *sql = "SELECT SUM(population) FROM countries;";
-    long total = 0;
-    
-    if (sqlite3_prepare_v2(get_db_connection(), sql, -1, &stmt, NULL) == SQLITE_OK) {
-        if (sqlite3_step(stmt) == SQLITE_ROW) {
-            total = sqlite3_column_int64(stmt, 0);
-        }
-        sqlite3_finalize(stmt);
-    }
-    return total;
 }
